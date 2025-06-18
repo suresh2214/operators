07e43c3822b157ee3e2d611ac1a1f37bf92265da
suresh_999 <babusureshoruganti@gmail.com>
2025-06-18 14:45:27 +0530

    Create Unaryoperators.java

---
+++ Unaryoperators.java
@@ -0,0 +1,8 @@
+public class UnaryOperators {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("++x: " + (++x)); // 6
+        System.out.println("x--: " + (x--)); // 6 (then x becomes 5)
+        System.out.println("x: " + x);       // 5
+    }
+}
