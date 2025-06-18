09e6fedb016c8aa9d101c16bd89c1eb3399765f7
suresh_999 <babusureshoruganti@gmail.com>
2025-06-18 14:40:15 +0530

    Create arithematic.java

---
+++ arithematic.java
@@ -0,0 +1,13 @@
+public class ArithmeticExample
+{
+public static void main (String[]args)
+{
+int a=10,b=9;
+System.out.println("a+b="+(a+b));
+System.out.println("a-b="+(a-b));
+System.out.println("a*b="+(a*b));
+System.out.println("a/b="+(a/b));
+System.out.println("a%b="+(a%b));
+}
+}
+
